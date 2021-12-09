b50a8bfd10c86e4c5dba0c06e5c8b04235de01c7
Reqtiade <j.florberg@gmail.com>
2021-12-09 13:31:11 +0100

    added beginning of testing code for messageHandler (cant run any functions tho)

--- Orchestrator/messageHandler.go
+++ Orchestrator/messageHandler.go
@@ -1,1 +1,22 @@
 package orchestrator
+
+import (
+	"encoding/json"
+	"fmt"
+	forms "orchestrator/forms"
+)
+
+func test() {
+	var serviceRequestForm forms.ServiceRequestForm
+
+	serviceRequestForm.RequesterSystem.SystemName = "Jacks dator"
+}
+
+func constructMessage() {
+
+}
+
+func parseMessage(serviceRequestForm forms.ServiceRequestForm) {
+	res, _ := json.Marshal(serviceRequestForm)
+	fmt.Println(res)
+}
