ce9a6c056c2ec3db8d866dc89fabadb7148a2040
Mehdows <marcus99.ma@gmail.com>
2021-12-09 17:52:45 +0100

    Finished client

--- Orchestrator/client.go
+++ Orchestrator/client.go
@@ -1,1 +1,45 @@
 package Orchestrator
+
+import(
+	"net/http"
+	"bytes"
+	"io/ioutil"
+)
+
+
+//Client struct
+type client struct {
+	httpAdrs string
+}
+
+
+//Init Client
+func newClient(httpAdrs string) *client {
+	c := client{httpAdrs: httpAdrs}
+	return &c
+}
+
+//	SendJson()
+//	Sends Json file to target http server
+//	returns server response as json
+func SendJson(c client, jsonStr []byte) []byte{
+
+	resp, err := http.Post(c.httpAdrs, "application/json", bytes.NewBuffer(jsonStr))
+	
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		//Failed to read response.
+		panic(err)
+	}
+
+	result := []byte(body)
+
+	return result
+}
+
